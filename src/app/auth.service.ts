064905442db72bbe5cb8cab18291da5d4513562b
gaurav305 <sainigaurav305@gmail.com>
2020-06-20 23:30:21 +0530

    Initial commit

---
+++ src/app/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+
+  getuserdetail(){
+    //post this to API server, return user info if correct
+  }
+}
